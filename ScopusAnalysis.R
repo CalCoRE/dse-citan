093a48c588f6ec69bb653574ffe457c1354822e1
Michelle Wilkerson <michellehoda@users.noreply.github.com>
2023-06-07 22:20:43 -0700

    cleaner merge
    Cleaner merged records; looking at historical nets

--- ScopusAnalysis.R
+++ ScopusAnalysis.R
@@ -30,6 +30,7 @@ mergedScopus <- rbind(scopusPrimary, scopusSecondary)
 # figure out what columns in common for scopus and wos, then merge
 commonCols <- intersect(names(mergedScopus), names(wos))
 allRecords <- rbind(mergedScopus[c(commonCols)],wos[c(commonCols)])
+#cleanRecords <- mergeDbSources(mergedScopus, wos)
 
 # DATA CLEANING
 
@@ -45,12 +46,12 @@ cleanRecords <- cleanRecords[ ! cleanRecords$UT %in% stoplist, ]
 
 ### Take all these reports and other pubs that say they don't have a
 ### title and duplicate the SO col to TI for interpretability
-titlelist <- scan("title-list.txt", what="", sep="\n")
-for(ut in titlelist) { cleanRecords["TI"][cleanRecords["UT"]==ut] <- cleanRecords["SO"][cleanRecords["UT"]==ut] }
+#titlelist <- scan("title-list.txt", what="", sep="\n")
+#for(ut in titlelist) { cleanRecords["TI"][cleanRecords["UT"]==ut] <- cleanRecords["SO"][cleanRecords["UT"]==ut] }
 
 # CLEAN UP
 
-rm(mergedScopus, allRecords, commonCols, stoplist, titlelist, ut)
+rm(mergedScopus, allRecords, stoplist)
 
 # OUTPUT SUMMARY RESULTS
 
@@ -66,15 +67,19 @@ summary(results, max=10)
 netMatrix <- biblioNetwork(as.data.frame(cleanRecords), analysis = "co-citation", network = "references", sep = ";")
 net20=networkPlot(netMatrix, n = 20, Title = "Co-Citation Network of Top 20 Cited Papers",  size.cex=TRUE, size=15, remove.multiple=FALSE, labelsize=.5,edgesize = 5, edges.min=0, type = "fruchterman")
 net50=networkPlot(netMatrix, n = 50, Title = "Co-Citation Network of Top 50 Cited Papers",  size.cex=TRUE, size=15, remove.multiple=FALSE, remove.isolates = TRUE, labelsize=.5,edgesize = 5, edges.min=0, type = "fruchterman")
+net200=networkPlot(netMatrix, n = 200, Title = "Co-Citation Network of Top 200 Cited Papers",  size.cex=TRUE, size=10, remove.multiple=FALSE, remove.isolates = TRUE, labelsize=.2, edgesize = 5, edges.min=0, type = "fruchterman")
+net500=networkPlot(netMatrix, n = 500, Title = "Co-Citation Network of Top 500 Cited Papers",  size.cex=TRUE, size=15, remove.multiple=FALSE, remove.isolates = TRUE, labelsize=.5,edgesize = 5, edges.min=0, type = "fruchterman")
 
 # Journal co-citation network analysis
 # This represents the top 15 publications (e.g. journals, books, reports) cited by
 # papers yielded in our Scopus search. Importantly, JLS is in the top 5.
 # NOTE extracting seems to be really broken. moving on.
 
-#extract=metaTagExtraction(data.frame(cleanRecords),"CR_SO",sep=";")
-#sources <- biblioNetwork(extract, analysis = "co-citation", network = "sources", sep = ";")
-#net=networkPlot(sources, n = 15, Title = "Co-Citation Network", type = "auto", size.cex=TRUE, size=15, remove.multiple=FALSE, labelsize=1,edgesize = 5, edges.min=2)
+extract=metaTagExtraction(data.frame(cleanRecords),"CR_SO",sep=";")
+sources <- biblioNetwork(extract, analysis = "co-citation", network = "sources", sep = ";")
+net=networkPlot(sources, n = 15, Title = "Co-Citation Network", type = "auto", size.cex=TRUE, size=15, remove.multiple=FALSE, labelsize=1,edgesize = 5, edges.min=2)
+
+conceptualStructure(data.frame(cleanRecords))
 
 # historical analysis - does not seem to yield anything too interesting
 
@@ -83,17 +88,10 @@ net50=networkPlot(netMatrix, n = 50, Title = "Co-Citation Network of Top 50 Cite
 keywordMatrix <- biblioNetwork(mergedScopus, analysis = "co-occurrences", network = "keywords", sep = ";")
 net=networkPlot(keywordMatrix, normalize="association", n = 50, Title = "Keyword Co-occurrences", type = "fruchterman", size.cex=TRUE, size=20, remove.multiple=F, edgesize = 10, labelsize=5,label.cex=TRUE,label.n=30,edges.min=2)
 
-# Thematic Map and cluster info based on keywords
-Map=thematicMap(data.frame(cleanRecords), field = "ID", n = 250, minfreq = 4, stemming = FALSE, size = 0.7, n.labels=5, repel = TRUE)
-plot(Map$map)
-Clusters=Map$words[order(Map$words$Cluster,-Map$words$Occurrences),]
-
-CL <- Clusters %>% group_by(.data$Cluster_Label) %>% top_n(5, .data$Occurrences)
-print(CL,n=40)
-
 # Author collaborations - not super useful because of large symposia
 
-#NetMatrix <- biblioNetwork(data.frame(cleanRecords), analysis = "collaboration",  network = "authors", sep = ";")
-#net=networkPlot(NetMatrix,  n = 50, Title = "Author collaboration",type = "auto", size=10,size.cex=T,edgesize = 3,labelsize=1)
+NetMatrix <- biblioNetwork(data.frame(cleanRecords), analysis = "collaboration",  network = "authors", sep = ";")
+net=networkPlot(NetMatrix,  n = 50, Title = "Author collaboration",type = "auto", size=10,size.cex=T,edgesize = 3,labelsize=1)
 
-histResults <- histNetwork(data.frame(cleanRecords), sep = ";")
+histResults <- histNetwork(as.data.frame(cleanRecords), sep = ";")
+plot(histResults)
