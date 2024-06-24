214a5a0b55464c9fd8bd944b4f0a4cd1498c7fe7
Michelle Wilkerson <michellehoda@users.noreply.github.com>
2024-06-23 22:36:22 -0700

    Update dse_citan_sneakpeek.R
    trees working to aggregate children and locate main parent ref text.
    
    breaks when applied at the dataframe level, though...

--- dse_citan_sneakpeek.R
+++ dse_citan_sneakpeek.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(stringdist) # for reference cleanup
 library(stringr)
 library(textTools)
+library(data.tree)
 
 source("dse_citan.R")
 
@@ -23,6 +24,10 @@ coreDSEworks <- getCoreDSEWorks()
 refWorks <- as.data.frame(
   citations(coreDSEworks, field = "article", sep = ";")$Cited)
 
+# cement these id's in case the indices get shuffled.
+# matching info will refer to these identifiers.
+refWorks$id <- 1:nrow(refWorks)
+
 # The reference list has some disasterous typos that cause several citations
 # to be missed or mis-mapped by bibliometrix. I cleared them out using
 # stringdist to build a lookup table of typos back to the most frequently
@@ -31,28 +36,66 @@ refWorks <- as.data.frame(
 # using a saved csv.
 
 matches <- read.csv("matches5.csv")
+matches$X <- NULL
 
-# Uncomment the lines below to recalculate the matrix (for example, if you
-# have updated your coreDSEworks).
+# Uncomment the lines below to reconstruct the matrix (for example, if you
+# have updated your coreDSEworks). It takes quite a while... don't forget to
+# save your new matches!
 
 # matches <- as.data.frame(which(stringdist::stringdistmatrix(
 #   refWorks[["CR"]], refWorks[["CR"]]) < 5, arr.ind=TRUE)) %>%
 #   filter(row<col)
-# colnames(matches) <- c("main","dupe")
 
-cleanRefsLookup <- data.frame()
+colnames(matches) <- c("main","dupe")
 
+# cleanRefsLookup <- data.frame()
 # Consolidate citations from dups to the main - get it done way
-for( i in 1:nrow(matches) ) {
-  refWorks$Freq[matches$main[i]] <-
-    refWorks$Freq[matches$main[i]] +
-    refWorks$Freq[matches$dupe[i]]
-  print(paste("Adding",refWorks$Freq[matches$dupe[i]],"to",refWorks$CR[matches$main[i]]))
-  refWorks$Freq[matches$dupe[i]] <- 0
-  cleanRefsLookup <- rbind(cleanRefsLookup,
-                           data.frame(refWorks$CR[matches$main[i]],
-                                      refWorks$CR[matches$dupe[i]]))
+# for( i in 1:nrow(matches) ) {
+#   refWorks$Freq[id=matches$main[i]] <-
+#     refWorks$Freq[id=matches$main[i]] +
+#     refWorks$Freq[id=matches$dupe[i]]
+#   print(paste("Adding",refWorks$Freq[id=matches$dupe[i]],"to",refWorks$CR[id=matches$main[i]]))
+#   refWorks$Freq[id=matches$dupe[i]] <- 0
+#   cleanRefsLookup <- rbind(cleanRefsLookup,
+#                            data.frame(refWorks$CR[id=matches$main[i]],
+#                                       refWorks$CR[id=matches$dupe[i]]))
+# }
+
+# do it a tree way. first, add the match forest to a 0th tree
+#forest <- data.frame(0,matches$main)
+#colnames(forest) <- c("main","dupe")
+forest <- matches
+head(forest$main,5)
+forest <- forest %>% bind_rows(data.frame(main=0,dupe=matches$main))
+searchTree <- data.tree::FromDataFrameNetwork(forest)
+
+## USE THE TREE TO GET CHILDREN OF EACH MAIN ITEM
+
+getAllDupRefs <- function(node,l=list()) {
+  if( length(node$children) > 0 ) { # check for more kiddos
+    l <- lapply(node$children, getAllDupRefs)
+  }
+  return( distinct(as.data.frame(unname(append(unlist(l),node$name)))) )
 }
+
+dupList <- function(node) {
+  myList <- getAllDupRefs(searchTree[[node]])
+  return( myList )
+}
+
+## USE THIS TO SUM THE CITES
+## USE THIS TO ASSIGN THE MOST FREQUENT CR
+agg <- function(node) {
+  myDupList <- dupList(node)
+  colnames(myDupList) <- "refId"
+  getSet <- refWorks %>% filter(id %in% myDupList$refId )
+  sum <- sum(getSet$Freq)
+  CR <- refWorks$CR[id=min(getSet$id)]
+  return( c(sum, as.character(CR) ) )
+}
+
+testResult <- forest %>% filter(main==0)
+testResult$Freq <- agg(as.character(testResult$dupe))
 
 colnames(cleanRefsLookup) <- c("main","dupe")
 
@@ -60,9 +103,6 @@ colnames(cleanRefsLookup) <- c("main","dupe")
 paste( "I found", count(matches),
        "dupes which leaves", count(refWorks) - count(matches),
        "real refs" )
-
-# and re-index the refWorks dataframe using the fixed reference counts
-refWorks <- refWorks %>% arrange(desc(Freq))
 
 for( i in i:nrow(coreDSEworks) ) {
   currentRefsList <- as.data.frame(str_split(coreDSEworks$CR[i],"; "))
@@ -74,17 +114,6 @@ for( i in i:nrow(coreDSEworks) ) {
   coreDSEworks$CR[i] <- paste(currentRefsList$ref,collapse="; ")
   print(paste(i,"Replaced"))
 }
-# #make a df from the refs
-# # NOTE currentRefsList is the thing I'd construct in the loop
-# currentRefsList <- as.data.frame(str_split(coreDSEworks$CR[2],"; "))
-# colnames(currentRefsList) <- c("ref")
-#
-# # replace dupes with mains
-# typoRefs <- match(currentRefsList$ref,cleanRefsLookup$dupe)
-# currentRefsList$ref[!is.na(typoRefs)] <- cleanRefsLookup$main[na.omit(match(currentRefsList$ref,cleanRefsLookup$dupe))]
-#
-# # put it back together
-# coreDSEworks$CR[2] <- paste(currentRefsList$ref,collapse="; ")
 
 # build co-citation network of DSE cited works
 refMatrix <- biblioNetwork(coreDSEworks, analysis = "co-citation",
@@ -105,7 +134,9 @@ net2VOSviewer(refNet,".")
 brokers <- refNet[["cluster_res"]] %>% filter( btw_centrality >= 100 )
 
 # Let's sort them by popularity
+############ BROKEN
 refWorks %>% filter(brokers %in% refWorks$CR)
+############
 
 # Now, look for "core" cluster membership of the referenced papers. These are
 # papers that have low betweenness centrality. Examining only these papers that
